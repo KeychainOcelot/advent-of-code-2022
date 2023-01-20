62cfdf611d47291742084ff3ccddfa7efc8bc5cd
KeychainOcelot <mysticshunal@gmail.com>
2023-01-20 22:46:43 +0530

    started day three

---
+++ three/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+
+int main(){
+    std::ofstream input("./input.txt");
+    std::vector<char> stuff;
+    while(!input.eof()) {
+        
+    }
+    return 0;
+}
